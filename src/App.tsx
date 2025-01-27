d05ef6711c9d3dc17f8268f9a8151ca8cb6c3eb2
Richard Essuman <ressuman001@gmail.com>
2025-01-27 16:32:48 +0000

    initialize React + TypeScript + Vite project with Tailwind CSS, Shadcn UI and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-slate-600">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
